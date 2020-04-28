967f566666ffdab002d981896f29f24b56dbeb9e
Jordi Mas <jmas@softcatala.org>
2020-04-28 21:11:35 +0200

    First version

--- indexer/indexcreator.py
+++ indexer/indexcreator.py
@@ -1,5 +1,4 @@
-# -*- encoding: utf-8 -*-
-#
+#-*- encoding: utf-8 -*-
 # Copyright (c) 2019 Jordi Mas i Hernandez <jmas@softcatala.org>
 #
 # This program is free software; you can redistribute it and/or
@@ -26,6 +25,12 @@ from whoosh.fields import BOOLEAN, TEXT, Schema, STORED, ID
 from whoosh.index import create_in
 from findfiles import FindFiles
 
+class MemoryEntry(object):
+    def __init__(self, word, filename, is_infinitive, infinitive):
+        self.word = word
+        self.filename = filename
+        self.is_infinitive = is_infinitive
+        self.infinitive = infinitive
 
 class IndexCreator(object):
 
@@ -33,6 +38,9 @@ class IndexCreator(object):
         self.json_dir = json_dir
         self.dir_name = "data/indexdir/"
         self.writer = None
+        self.duplicated = {}
+        self.memory_index = []
+
 
     def create(self, in_memory=False):
         tokenizer_pattern = rcompile(r"(\w|·)+(\.?(\w|·)+)*") # Includes l·l
@@ -71,9 +79,9 @@ class IndexCreator(object):
         return s
 
 
-    def write_entry(self, verb_form, file_path, infinitive):
+    def write_entry(self, verb_form, file_path, is_infinitive):
 
-        if infinitive is True:
+        if is_infinitive is True:
             index_letter = self._get_first_letter_for_index(verb_form)
         else:
             index_letter = None
@@ -82,21 +90,35 @@ class IndexCreator(object):
                                  file_path = file_path,
                                  index_letter = index_letter)
 
-    def _write_term(self, indexed, filename, word, form, infinitive):
-        print(filename)
-        print(word)
-        print(form)
-        print("---")
 
-        self.write_entry(word, filename, infinitive)
+    def _write_term(self, indexed, filename, word, form, is_infinitive, infinitive):
         indexed.add(word)
+        self.memory_index.append(MemoryEntry(word, filename, is_infinitive, infinitive))
+        return
+
+
+    def _get_duplicated_forms_across_verbs(self, data, infinitive, sps):
+        for form in data[infinitive]:
+            for sp in sps:
+                for conjugacio in form[sp]:
+                    word = conjugacio['word']
+                    if word in self.duplicated:
+                        infinitives = self.duplicated[word]
+                    else:
+                        infinitives = set()
+
+                    infinitives.add(infinitive)
+                    self.duplicated[word] = infinitives
 
 
     def _process_file(self, filename):
         with open(filename) as json_file:
             data = json.load(json_file)
             infinitive = list(data.keys())[0]
-            
+            sps = ['singular1', 'singular2', 'singular3', 'plural1', 'plural2', 'plural3']
+
+            self._get_duplicated_forms_across_verbs(data, infinitive, sps)
+
             #if infinitive != 'cantar':
             #    return 0
 
@@ -113,22 +135,50 @@ class IndexCreator(object):
 
                         words = [x.strip() for x in word.split('/')]
                         for word in words:
-                            infinitive = form['form'] == "Infinitiu"
-                            self._write_term(indexed, filename, word, form['form'], infinitive)
-
-        return len(indexed)
+                            is_infinitive = form['form'] == "Infinitiu"
+                            self._write_term(indexed, filename, word, form['form'], is_infinitive, infinitive)
 
     def save_index(self):
         self.writer.commit()
 
+    def _add_infinitive_to_duplicates(self):
+        duplicated = 0
+        for word in self.duplicated.keys():
+            infinitives = self.duplicated[word];
+            if len(infinitives) <= 1:
+                continue
+
+            for infinitive in infinitives:
+                for entry in self.memory_index:
+                    if entry.word == word and entry.infinitive == infinitive:
+                        entry.word = "{0} ({1})".format(word, infinitive)
+
+            duplicated = duplicated + 1
+
+        return duplicated
+
+
+    def _save_memory_index(self):
+        indexed = set()
+        for entry in self.memory_index:
+            self.write_entry(entry.word, entry.filename, entry.infinitive)
+            print(entry.filename)
+            print(entry.word)
+            print(entry.infinitive)
+            print("---")
+            indexed.add(entry.word)
+
+        return len(indexed)
 
     def process_files(self):
         findFiles = FindFiles()
         files = findFiles.find_recursive(self.json_dir, '*.json')
-        indexed = 0
         for filename in files:
-            indexed += self._process_file(filename)
+            self._process_file(filename)
 
-        print("Processed {0} files, indexed {1} variants".format(len(files), indexed))
+        duplicated = self._add_infinitive_to_duplicates()
+        indexed = self._save_memory_index()
 
+        print("Processed {0} files, indexed {1} variants, duplicated forms across verbs {2}".
+             format(len(files), indexed, duplicated))
 
